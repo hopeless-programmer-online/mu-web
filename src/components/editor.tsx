58e7075a2c330a4dd260635ffcbc8d32e8bf60e7
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2024-10-17 16:11:57 +0300

    Update editor.tsx

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,17 +1,52 @@
 import React from 'react'
 import style from './editor.module.scss'
 
-export type Props = {}
-export type State = unknown
+export type Props = unknown
+export type State = {
+    source : string
+    log : string
+}
 
 export default class EditorComponent extends React.Component<Props, State> {
+    public state : State = {
+        source : ``,
+        log : ``,
+    }
+
+    private onRun = () => {
+        const { source } = this.state
+
+        let log = ``
+
+        try {
+            const begin = new Date
+
+            const end = new Date
+
+            log += `completed in ${((end.valueOf() - begin.valueOf()) / 1_000).toFixed(2)}sec\n`
+            log += `done\n`
+        }
+        catch (error) {
+            log += String(error)
+
+            console.error(error)
+        }
+
+        this.setState({ log })
+    }
+    private onSourceChanged = ({ currentTarget : { value : source } } : React.ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({ source })
+    }
+
     public render() {
+        const { source, log } = this.state
+
         return (
             <article className={style.editor}>
                 <header>
                     <h2>Online compiler</h2>
                     <button
-                        // onClick={this.onRun}
+                        onClick={this.onRun}
                     >
                         Run
                     </button>
@@ -21,8 +56,8 @@ export default class EditorComponent extends React.Component<Props, State> {
                         Source
                     </figcaption>
                     <textarea
-                        // value={source}
-                        // onChange={this.onSourceChanged}
+                        value={source}
+                        onChange={this.onSourceChanged}
                     />
                 </figure>
                 <figure>
@@ -30,7 +65,7 @@ export default class EditorComponent extends React.Component<Props, State> {
                         Output
                     </figcaption>
                     <textarea
-                        // value={log}
+                        value={log}
                         readOnly
                     />
                 </figure>
